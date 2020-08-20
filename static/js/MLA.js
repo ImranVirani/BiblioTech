1ed5fa443c23a281447e1074539dd97e4e38b9ce
potatoman16 <virani.imran@icloud.com>
2020-08-20 17:29:21 -0400

    Fixed functionality of MLA

--- static/js/MLA.js
+++ static/js/MLA.js
@@ -12,4 +12,5 @@ function displayEntry() {
     var entry = document.getElementById("result-entry").innerHTML += authorLastName + ", " + authorFirstName + '. "' + articleTitle + '." ' + "<em>" + publisherName + "</em>" + " " + dayPublished + " " + monthPublished + " " + yearPublished + " " + urlOfSource + " " + "Accessed " + dateOfAccess;
     document.getElementById("result-entry").innerHTML += "<br>";
 }
+var test = "hello";
 document.getElementById("submit-button").addEventListener("click", displayEntry);
