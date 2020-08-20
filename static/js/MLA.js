47448dd0bcdde0bf9277d85de08367958a0c0f74
potatoman16 <virani.imran@icloud.com>
2020-08-20 17:01:02 -0400

    Separated functionality of each type of entry

---
+++ static/js/MLA.js
@@ -0,0 +1,29 @@
+function displayEntry() {
+    var authorFirstName = document.getElementById("authorFirstName").value;
+    var authorLastName = document.getElementById("authorLastName").value;
+    var articleTitle = document.getElementById("articleTitle").value;
+    var publisherName = document.getElementById("publisherName").value;
+    var dayPublished = document.getElementById("dayPublished").value;
+    var monthPublished = document.getElementById("monthPublished").value;
+    var yearPublished = document.getElementById("yearPublished").value;
+    var urlOfSource = document.getElementById("urlOfSource").value;
+    var dateOfAccess = document.getElementById("dateOfAccess").value;
+    var entryNumber = 1;
+    if (formatType = "mla") {
+        var entry = document.getElementById("result-entry").innerHTML += authorLastName + ", " + authorFirstName + '. "' + articleTitle + '." ' + "<em>" + publisherName + "</em>" + " " + dayPublished + " " + monthPublished + " " + yearPublished + " " + urlOfSource + " " + "Accessed " + dateOfAccess;
+
+    }
+    // Checks to see which type of citations is being done
+    /*if (formatType = "apa") {
+        var entry = document.getElementById("result-entry" + entryNumber).innerHTML += authorLastName + ", " + authorFirstName[0] + " (" + yearPublished + "," + " " + monthPublished + " " + dayPublished + "). " + "<em>" + articleTitle + "</em>." + " " + publisherName + ". " + urlOfSource
+
+    }
+    // Chicago has to be done
+    if (formatType = "chicago") {
+        var entry = document.getElementById("result-entry" + entryNumber).innerHTML += authorLastName + ", " + authorFirstName + '. "' + articleTitle + '." ' + "<em>" + publisherName + "</em>" + " " + dayPublished + " " + monthPublished + " " + yearPublished + " " + urlOfSource + " " + "Accessed " + dateOfAccess;
+
+    }
+    */
+    document.getElementById("result-entry").innerHTML += "<br>";
+}
+document.getElementById("submit-button").addEventListener("click", displayEntry);
